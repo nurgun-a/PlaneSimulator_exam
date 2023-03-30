af817a906bfb1b689e6cd0810ec6b2231efe93f5
nurgun_a <nurgun.alekseev@bk.ru>
2023-03-30 20:40:18 +0300

    Добавьте файлы проекта.

---
+++ PlaneSimulator_exam/Plane.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using static System.Console;
+using System.IO;
+
+
+namespace PlaneSimulator_exam
+{
+    //if (SpeedChanged != null) SpeedChanged(this, speed);
+    //throw new Exception("Самолет разбился");
+    class Plain
+    {
+        public int Speed { get; private set; }
+        public int Height { get; private set; }
+        public int Penalty_points { get; set; }
+        public double Distance { get; private set; }
+        public delegate void ChangeParameterEventHandler(int value);
+        public event ChangeParameterEventHandler SpeedChanged;
+        public event ChangeParameterEventHandler HeightChanged;
+
+        public Plain()
+        {
+            Speed = 0;
+            Height = 0;
+            Distance = 0;
+            SpeedChanged += OnSpeedChanged;
+            HeightChanged += OnHeightChanged;
+        }
+        public void Land()
+        {
+            if (Speed == 0 && Height == 0)
+            {
+                WriteLine("Ок");
+            }
+            else
+            {
+                throw new Exception("Вы не смогли прземлиться. Самолет разбился");
+            }
+
+        }
+
+        public void IncreaseSpeed(int value)
+        {
+            if (Speed < 1000)
+            {
+                Speed += value;
+                
+                if (SpeedChanged != null) SpeedChanged(value);
+            }
+        }
+
+        public void ReduceSpeed(int value)
+        {
+            if (Speed > 50)
+            {
+                Speed -= value;
+                if (Speed < 0)
+                {
+                    Speed = 0;
+                }
+                if (SpeedChanged != null) SpeedChanged(value);              
+            }
+        }
+
+        public void IncreaseHeight(int value)
+        {
+            if (Height < 10000)
+            {
+                Height += value;
+                
+                if (HeightChanged != null) HeightChanged(value);
+            }
+        }
+
+        public void ReduceHeight(int value)
+        {
+            if (Height > 50)
+            {
+                Height -= value;
+                if (Height<0)
+                {
+                    Height = 0;
+                }
+                if (HeightChanged != null) HeightChanged(value);
+            }
+        }
+
+        private void OnSpeedChanged(int value)
+        {
+            Log($"Скорость: {Speed} км/ч, изменение скорости на {value} км/ч {DateTime.Now}");
+        }
+
+        private void OnHeightChanged(int value)
+        {
+            Log($"Высота: {Height} м, изменение высоты на {value} м {DateTime.Now}");
+        }
+
+        public void OnDistanceChanged(double value)
+        {
+            Distance += (value * Speed);
+            WriteLine($"Пройденное растояние {Distance} км.");
+        }
+
+        public void Log(string str)
+        {
+            string fPath = "Log.txt";
+            using (FileStream fs = new FileStream(fPath, FileMode.Append))
+            {
+                using (StreamWriter sw = new StreamWriter(fs, Encoding.Unicode))
+                {
+                    sw.WriteLine(str);
+                }               
+            }
+        }
+    }
+
+
+
+    class Dispatcher
+    {
+        private string name;
+        private int controlDistanceStart;
+        private int controlDistanceEnd;
+        private int correction;
+
+        public Dispatcher(string name, int controlDistanceStart, int controlDistanceEnd = 0)
+        {
+            this.name = name;
+            this.controlDistanceStart = controlDistanceStart;
+            this.controlDistanceEnd = controlDistanceEnd;
+            Random rand = new Random();
+            correction = rand.Next(-200, 201); //генерация корректировки погодных условий
+        }
+
+        public bool Control(Plain plain)
+        {
+            if (plain.Distance >= controlDistanceStart && plain.Distance <= controlDistanceEnd )
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        public void OnSpeedChanged(Plain plain)
+        {
+            int recommendedHeight = 7 * plain.Speed - correction;
+            if (plain.Distance<8000)
+            {
+                
+
+                int deviation = Math.Abs((recommendedHeight <= 10000 ? recommendedHeight : 10000) - plain.Height);
+                if (deviation < 300)
+                {
+                    WriteLine($"\n\nДиспетчер {name}: Рекомендуемая высота: {(recommendedHeight <= 0 ? 0 : recommendedHeight)} м, текущая высота: {plain.Height} метров");
+                }
+                else if (deviation < 600)
+                {
+                    WriteLine($"\n\nДиспетчер {name}: Рекомендуемая высота: {(recommendedHeight <= 0 ? 0 : recommendedHeight)} м, текущая высота: {plain.Height} метров, Штраф: 25 points");
+                    plain.Penalty_points += 25;
+                }
+                else if (deviation < 1000)
+                {
+                    WriteLine($"\n\nДиспетчер {name}: Рекомендуемая высота: {(recommendedHeight <= 0 ? 0 : recommendedHeight)} м, текущая высота: {plain.Height} метров, Штраф: 25 points");
+                    plain.Penalty_points += 50;
+                }
+                else if (deviation > 1000)
+                {
+                    plain.Log("\n\nРазница с рекомендуемой высотой 1000 метров.Самолет разбился\n\n");
+                    throw new Exception("Разница с рекомендуемой высотой 1000 метров. Самолет разбился");
+                }
+                if (plain.Penalty_points >= 1000)
+                {
+                    plain.Log("\n\nВы не справились, начислено 1000 штрафных очков.\n\n");
+                    throw new Exception("Вы не справились, начислено 1000 штрафных очков.");
+                }
+            }
+
+            else
+            {
+                recommendedHeight -= plain.Speed;
+
+                int deviation = Math.Abs((recommendedHeight <= 10000 ? recommendedHeight : 10000) - plain.Height);
+                if (deviation < 300)
+                {
+                    WriteLine($"\n\nДиспетчер {name}: Рекомендуемая высота: {(recommendedHeight <= 0 ? 0 : recommendedHeight)} м, текущая высота: {plain.Height} метров");
+                }
+                else if (deviation < 600)
+                {
+                    WriteLine($"\n\nДиспетчер {name}: Рекомендуемая высота: {(recommendedHeight <= 0 ? 0 : recommendedHeight)} м, текущая высота: {plain.Height} метров, Штраф: 25 points");
+                    plain.Penalty_points += 25;
+                }
+                else if (deviation < 1000)
+                {
+                    WriteLine($"\n\nДиспетчер {name}: Рекомендуемая высота: {(recommendedHeight <= 0 ? 0 : recommendedHeight)} м, текущая высота: {plain.Height} метров, Штраф: 25 points");
+                    plain.Penalty_points += 50;
+                }
+                else if (deviation > 1000)
+                {
+                    plain.Log("\n\nРазница с рекомендуемой высотой 1000 метров.Самолет разбился\n\n");
+                    throw new Exception("Разница с рекомендуемой высотой 1000 метров. Самолет разбился");
+                }
+                if (plain.Penalty_points >= 1000)
+                {
+                    plain.Log("\n\nВы не справились, начислено 1000 штрафных очков.\n\n");
+                    throw new Exception("Вы не справились, начислено 1000 штрафных очков.");
+                }
+            }
+            
+        }
+
+    }
+}
+
---
+++ PlaneSimulator_exam/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+using static System.Console;
+
+namespace PlaneSimulator_exam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Plain plain = new Plain();
+            
+            Timer_class ts = new Timer_class();           
+            Thread th1 = new Thread(ts.Timer_);
+           
+
+            Dispatcher dispatcher1 = new Dispatcher("Dispatcher 1", 0, 5000);
+            Dispatcher dispatcher2 = new Dispatcher("Dispatcher 2", 5001, 10000);
+
+            Random rand = new Random();
+            Thread.Sleep(1000);
+            th1.Start();
+
+            try
+            {
+                while (plain.Distance < 10000)
+                {
+
+                    //Thread.Sleep(1000); 
+                    Clear();
+                    WriteLine("\n");
+                    int n1 = rand.Next(-200, 201);
+                    int n2 = rand.Next(-200, 201);
+
+                    if (dispatcher1.Control(plain)) { Show(plain, dispatcher1); }                 
+                    if (dispatcher2.Control(plain)) { Show(plain, dispatcher2); }
+                    plain.OnDistanceChanged(((double)ts.Min + ((double)ts.Sec / 60)));   
+
+                    switch (ReadKey(true).Key)
+                    {
+                        case ConsoleKey.LeftArrow: plain.ReduceSpeed(50); break;
+                        case ConsoleKey.RightArrow: plain.IncreaseSpeed(50); break;
+                        case ConsoleKey.UpArrow: plain.IncreaseHeight(50); break;
+                        case ConsoleKey.DownArrow: plain.ReduceHeight(50); break;
+                        case ConsoleKey.W: plain.IncreaseHeight(150); break;
+                        case ConsoleKey.S: plain.ReduceHeight(150); break;
+                        case ConsoleKey.A: plain.ReduceSpeed(50); break;
+                        case ConsoleKey.D: plain.IncreaseSpeed(50); break;
+                    }
+                }
+                th1.Abort();
+                //plain.Land();
+               
+            }
+            catch (Exception ex)
+            {
+                WriteLine(ex.Message);
+                th1.Abort();
+                ReadKey();
+            }
+            
+
+            ReadKey();
+        }
+        public static void Show(Plain pl, Dispatcher d)
+        {
+            d.OnSpeedChanged(pl);
+            WriteLine(@"
+                            
+                                      Скорость: " + $"{pl.Speed}" + @"
+                                        Высота: " + $"{pl.Height}" + @"
+                          Пойденное расстояние: " + $"{pl.Distance}" + @"  километров
+
+
+                                 Штрафние очки: " + $"{pl.Penalty_points}" + @"
+");
+        }
+
+        public class Timer_class
+        {
+            public int Min { get; set; }
+            public int Sec { get; set; }
+            public void Timer_()
+            {
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+                while (sw.Elapsed.Minutes != 3)
+                {
+                    //SetCursorPosition(0, 0);
+                    //WriteLine($"{sw.Elapsed.Minutes:00}:{sw.Elapsed.Seconds:00}");
+                    //SetCursorPosition(0, 0);
+                    Min = sw.Elapsed.Minutes;
+                    Sec = sw.Elapsed.Seconds;
+                    Thread.Sleep(1000);
+                    //ClearLine();
+                }
+                sw.Stop();
+            }
+
+        }
+        public static void ClearLine()
+        {
+            SetCursorPosition(0, 0);
+            Write(new string(' ', WindowWidth));
+            SetCursorPosition(0, 0);
+        }
+    }
+}
